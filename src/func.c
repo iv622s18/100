e10aea14d98c4240324dd05685be8ca4ac114d89
Ваше Имя <sheriodickenz@gmail.com>
2017-07-12 17:12:35 +0600

    Implement function check

--- src/func.c
+++ src/func.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "func.h"
+#include <time.h>
+#include <stdlib.h>
+int check(int take)
+{
+
+	if (take < 10 && take > 0)
+
+	    return 1;
+	else return 0;
+
+}
 
--- src/func.h
+++ src/func.h
@@ -1,1 +1,8 @@
+#ifndef FUNC_H
+#define FUNC_H
 
+int check(int take);
+int move(int numOfRemain);
+int getrand(int min, int max);
+
+#endif
